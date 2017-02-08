4f6fd58e87ee468fc6a6fa6d16bc794f8f36c712
Tõnis Kasekamp <toniskasekamp@gmail.com>
2017-02-08 15:48:13 +0200

    KCom object

---
+++ src/client/app/shared/com/index.ts
@@ -0,0 +1,4 @@
+/**
+ * This barrel file provides the export for the shared ComService.
+ */
+export * from './com.service';
---
+++ src/client/app/shared/com/kcom.ts
@@ -0,0 +1,12 @@
+/**
+ * Created by Tonis on 08.02.2017.
+ */
+interface KCom {
+  active: string,
+  rssi: number,
+  mcuReset: number,
+  trxReset: number,
+  batStatus: string,
+  dcdcStatus: string,
+  epsStatus: string
+}
